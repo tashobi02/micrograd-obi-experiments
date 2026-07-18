d99ab626d1bee38154f0ac511a585d790e72a139
tashobi02 <obiditbd@gmail.com>
2026-07-18 16:53:02 +0600

    NN Backpropagation

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -2,13 +2,14 @@
 class Value:
     """ stores a single scalar value and its gradient """
 
-    def __init__(self, data, _children=(), _op=''):
+    def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self.grad = 0
         # internal variables used for autograd graph construction
         self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op # the op that produced this node, for graphviz / debugging / etc
+        self.label = label
 
     def __add__(self, other):
         other = other if isinstance(other, Value) else Value(other)
